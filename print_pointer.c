846974f5ab4875f54f744d0aef402071386097cb
DagmawiKK <dagmawikassa1@gmail.com>
2023-11-17 22:47:26 +0300

    handling p

--- print_pointer.c
+++ print_pointer.c
@@ -8,7 +8,7 @@
  */
 int print_pointer(void *ptr)
 {
-		intptr_t address = (intptr_t)ptr;
+		uintptr_t address = (uintptr_t)ptr;
 		int count = 0;
 		count += put_string("0x");
 		count += print_number(address, 16, 'x');
